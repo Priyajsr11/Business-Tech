ebd745e02e29203adb66e9fb18ddd58ca73e5b7f
Priya Singh <priyajsr65@gmail.com>
2020-09-02 23:07:13 +0530

    Updates

---
+++ precache-manifest.0a26996a1786e0dbe5e8e8b156c00622.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e630da1b99b248f215061953146743",
+    "url": "/Business-Tech/index.html"
+  },
+  {
+    "revision": "2ef15a6ba9c29bc91659",
+    "url": "/Business-Tech/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "784932df2651808d15a7",
+    "url": "/Business-Tech/static/css/main.563c7a92.chunk.css"
+  },
+  {
+    "revision": "2ef15a6ba9c29bc91659",
+    "url": "/Business-Tech/static/js/2.fb06489a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Business-Tech/static/js/2.fb06489a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784932df2651808d15a7",
+    "url": "/Business-Tech/static/js/main.217141d3.chunk.js"
+  },
+  {
+    "revision": "d5ddeaac6fd66a6abc94",
+    "url": "/Business-Tech/static/js/runtime-main.9e1323dd.js"
+  },
+  {
+    "revision": "fe82dc931938f41e29f6ee899d89b99c",
+    "url": "/Business-Tech/static/media/Abt.fe82dc93.jpg"
+  },
+  {
+    "revision": "81d563a8f9ba2e7c114b3bab2c782c66",
+    "url": "/Business-Tech/static/media/Android.81d563a8.jpg"
+  },
+  {
+    "revision": "ce0de232b178321f21386e604e163288",
+    "url": "/Business-Tech/static/media/App.ce0de232.jpg"
+  },
+  {
+    "revision": "fe34ba39ff19e32ae01a7d2c8435b9f7",
+    "url": "/Business-Tech/static/media/Dig.fe34ba39.jpg"
+  },
+  {
+    "revision": "38cbea4e48225668d1b3ad8edb223fc5",
+    "url": "/Business-Tech/static/media/Onl.38cbea4e.jpg"
+  },
+  {
+    "revision": "239fb326bd662973c1ae6255ed23bc59",
+    "url": "/Business-Tech/static/media/Soft.239fb326.jpg"
+  },
+  {
+    "revision": "3592a8335512c110f3e5eb113605ad52",
+    "url": "/Business-Tech/static/media/Web.3592a833.jpg"
+  }
+]);
